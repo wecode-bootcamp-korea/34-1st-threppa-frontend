6af5000a3d20611ec79935da079f7c7ce151379f
jihoon <rec8730@gmail.com>
2022-06-28 00:17:22 +0900

    Modify - sider리뷰 반영

--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -1,15 +1,8 @@
 import React from "react";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import "./ProductList.scss";
 
 const ProductList = () => {
-  const navigate = useNavigate();
-  const goToProductDesc = () => {
-    navigate("/productdesc");
-    console.log("hello");
-  };
-  return <button onClick={goToProductDesc}>페이지 이동</button>;
+  return <button>이동</button>;
 };
 
 export default ProductList;
