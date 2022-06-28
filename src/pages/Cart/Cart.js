25d72fb4a9f91e1db89f3d59e84dc9be16a7b470
jihyun-jeon <jhplus13@naver.com>
2022-06-29 00:51:16 +0900

    Modify: post api호출 구현

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,32 +1,54 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import CartItem from "./components/CartItem";
 import "./Cart.scss";
 
 const Cart = () => {
+  const timerRef = useRef(0);
   const [cartData, setCartData] = useState([]);
-  const [totalPrice, setTotalPrice] = useState();
+  let totalPrice = cartData.reduce(
+    (acc, cur) => acc + +cur.price * cur.quantity,
+    0
+  );
   let deliveryFee = totalPrice >= 100000 ? "무료" : 5000;
 
   // <데이터 요청>
-  // 로그인 후 나온 토큰 보내야 함.
   // const getUserToken =
   //   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MTR9.uCo_nqlEeKoRuz9m6fhE9Mru4bMhLIuhFb7y0UWkq_E";
-
   useEffect(() => {
+    // fetch("http://192.168.225.215:8000/products/carts", {
+    //   method: "GET",
+    //   headers: {
+    //     Authorization: getUserToken,
+    //   },
+    // })
     fetch("data/cart.json")
-      // fetch("http://192.168.225.215:8000/products/carts", {
-      //   method: "GET",
-      //   headers: {
-      //     Authorization: getUserToken,
-      //   },
-      // })
       .then(res => res.json())
       .then(result => {
-        const sumPrice = result.reduce((acc, cur) => acc + +cur.price, 0);
-        setTotalPrice(sumPrice);
+        // console.log(result);
+
         setCartData(result);
       });
   }, []);
+
+  useEffect(() => {
+    clearTimeout(timerRef.current);
+
+    timerRef.current = setTimeout(() => {
+      const postData = cartData.map(({ product_id, color, quantity, size }) => {
+        return { product_id, color, quantity, size };
+      });
+      console.log(postData);
+      // fetch("#", {
+      //   method: "POST",
+      //   headers: {
+      //     Authorization: "token",
+      //   },
+      //   body: postData,
+      // })
+      //   .then()
+      //   .then(result => console.log(result));
+    }, 2000);
+  }, [cartData]);
 
   // <수량조절 함수>
   const onClickQuantity = (itemId, itemQuantity, price, itemValue) => {
@@ -34,29 +56,45 @@ const Cart = () => {
       return;
     }
 
-    const updataData = cartData.map(data => {
-      if (data.product_id === itemId) {
-        if (itemValue === "minus") {
-          data.quantity -= 1;
-          setTotalPrice(prev => prev - price);
-          deliveryFee = totalPrice - price >= 100000 ? "무료" : 5000;
-        } else {
-          data.quantity += 1;
-          setTotalPrice(prev => prev + price);
-          deliveryFee = totalPrice + price >= 100000 ? "무료" : 5000;
-        }
-      }
-      return data;
-    });
+    const foundIdx = cartData.findIndex(data => data.product_id === itemId);
 
-    setCartData(updataData);
+    if (foundIdx === -1) {
+      return;
+    }
+
+    const newItem = { ...cartData[foundIdx] };
+
+    if (itemValue === "minus") {
+      newItem.quantity -= 1;
+    } else {
+      newItem.quantity += 1;
+    }
+
+    // const updataData = cartData.map(data => {
+    //   if (data.product_id === itemId) {
+    //     if (itemValue === "minus") {
+    //       data.quantity -= 1;
+    //
+    //     } else {
+    //       data.quantity += 1;
+    //       deliveryFee = totalPrice + price >= 100000 ? "무료" : 5000;
+    //     }
+    //   }
+    //   return data;
+    // });
+
+    setCartData(prev => {
+      const updataData = [...prev];
+      updataData[foundIdx] = newItem;
+
+      return updataData;
+    });
   };
 
   // <삭제 함수>
-  const onClickDelete = (product_id, price, quantity) => {
+  const onClickDelete = product_id => {
     const updataData = cartData.filter(data => data.product_id !== product_id);
     setCartData(updataData);
-    setTotalPrice(prev => prev - price * quantity);
   };
 
   return (
