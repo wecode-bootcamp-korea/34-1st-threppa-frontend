dec4277403549284767cab2546b23423cf5d49d6
jihyun-jeon <jhplus13@naver.com>
2022-06-28 09:58:27 +0900

    Modify:수량변경, 총액계산  기능구현

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,31 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Item from "./components/Item";
 import "./Cart.scss";
 
 const Cart = () => {
-  // fetch("data/cart.json")
-  //   .then(res => res.json())
-  //   .then(result => console.log(result));
+  // {id: 2, name: 'Clog2', price: 30000, quantity: 1}
+
+  const [cartData, setCartData] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    fetch("data/cart.json")
+      .then(res => res.json())
+      .then(result => {
+        setCartData(result);
+        const firstTotalPrice = result.reduce((acc, cur) => acc + cur.price, 0);
+        setTotalPrice(firstTotalPrice);
+      });
+  }, []);
 
   return (
     <div className="CartContainer">
       <h1 className="cartTitle">
-        <span className="bolder">장바구니</span> (1개수량)
+        <span className="bolder">장바구니</span> ({cartData.length}개수량)
       </h1>
       <div className="columnWrapper">
         <div className="column">
-          <p className="totalPrice">구매 상품 총액 : ₩89,800</p>
+          <p className="totalPrice">구매 상품 총액 : ₩{totalPrice}</p>
           {/* 컴포넌트 */}
-          <Item />
-          <Item />
-          <Item />
+          {cartData.map(obj => (
+            <Item
+              key={obj.id}
+              id={obj.id}
+              name={obj.name}
+              color={obj.color}
+              size={obj.size}
+              price={obj.price}
+              quantity={obj.quantity}
+              setCartData={setCartData}
+              setTotalPrice={setTotalPrice}
+            />
+          ))}
         </div>
         <div className="column">
           <h2 className="orderFormTitle">주문 내역</h2>
           <div className="orderLists">
             <ul className="orderList">
               <li className="listTitle">구매 상품 총액 </li>
-              <li className="listContent">₩179,700 </li>
+              <li className="listContent">₩{totalPrice}</li>
             </ul>
             <ul className="orderList">
               <li>쿠폰 코드 입력 </li>
@@ -39,12 +60,12 @@ const Cart = () => {
             </ul>
             <ul className="orderList">
               <li>배송비 </li>
-              <li>무료</li>
-              <li className="three">3만원 이상 무료입니다.</li>
+              <li>{totalPrice >= 50000 ? "무료" : "₩5000"}</li>
+              {/* <li className="three">5만원 이상 무료입니다.</li> */}
             </ul>
             <ul className="orderList">
               <li>주문 총액</li>
-              <li>₩179,700 </li>
+              <li>₩{totalPrice} </li>
             </ul>
           </div>
           <button type="button" className="orderBtn">
