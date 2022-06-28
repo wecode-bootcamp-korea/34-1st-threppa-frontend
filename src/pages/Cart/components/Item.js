00694f47a2e78c40af302403adb8be5c35c98bf3
jihyun-jeon <jhplus13@naver.com>
2022-06-28 09:58:27 +0900

    Modify:수량변경, 총액계산  기능구현

--- src/pages/Cart/components/Item.js
+++ src/pages/Cart/components/Item.js
@@ -1,38 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Item.scss";
 
-const Item = () => {
+const Item = ({
+  name,
+  id,
+  color,
+  size,
+  price,
+  quantity,
+  setCartData,
+  setTotalPrice,
+}) => {
+  const [num, setNum] = useState(quantity);
+
+  const onClickDel = () => {
+    setTotalPrice(prev => prev - price);
+    setCartData(prev => prev.filter(el => el.id !== id));
+  };
+
+  const minus = () => {
+    setNum(prev => {
+      // 갯수가 0이되면 해당상품 삭제
+      if (prev - 1 === 0) {
+        setTotalPrice(prev => prev - price);
+        setCartData(prev => prev.filter(el => el.id !== id));
+        return;
+      }
+      // 아니면 숫자 마이너스 처리
+      setTotalPrice(prev => prev - price);
+      return prev - 1;
+    });
+  };
+
+  const add = () => {
+    setNum(prev => prev + 1);
+    setTotalPrice(prev => prev + price);
+  };
+
   return (
     <div className="itemContainer">
       <div className="itemWrapper">
         <img src="images/shoe1.jpg" alt="shoe1" className="itemImg" />
         <div className="itemInfo">
           <dl className="itemTxt">
-            <dt>클래식 크록스 마블 샌들</dt>
+            <dt>{name}</dt>
             <dd>
-              색상 : <span className="underLine">파파야 멀티</span>
+              색상 : <span className="underLine">{color}</span>
             </dd>
             <dd>
-              사이즈 : <span className="underLine">230</span>
+              사이즈 : <span className="underLine">{size}</span>
             </dd>
           </dl>
           <div className="countBtns">
-            <button type="number" className="countBtn">
+            <button type="number" className="countBtn" onClick={minus}>
               -
             </button>
-            <span className="num">1</span>
-            <button type="number" className="countBtn">
+            <span className="num">{num}</span>
+            <button type="number" className="countBtn" onClick={add}>
               +
             </button>
           </div>
           <ul className="modify">
-            <li className="delBtn">삭제</li>
+            <li className="delBtn" onClick={onClickDel}>
+              삭제
+            </li>
             <li>주문수정</li>
           </ul>
         </div>
       </div>
 
-      <p className="unitPrice">₩44,900</p>
+      <p className="unitPrice">₩{price}</p>
     </div>
   );
 };
